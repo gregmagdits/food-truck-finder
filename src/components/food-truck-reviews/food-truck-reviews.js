9208bc3914700af18defef0ec660f56738c9dd9a
Greg Magdits <gregmagdits@gmail.com>
2019-10-19 11:01:55 -0400

    Added food-truck-review component

---
+++ src/components/food-truck-reviews/food-truck-reviews.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import "./food-truck-reviews.scss"
+import GoogleMapReact from "google-map-react";
+import {secrets} from "../../secrets";
+
+class FoodTruckReviews extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            foodTruck : props.foodTruck
+        }
+        console.log("foodTruck: ", this.state.foodTruck)
+    }
+    render(){
+        if (!this.state.foodTruck){
+            return <div>Loading....</div>
+        }
+
+        let reviews = this.state.foodTruck.reviews.map(review => {
+            return (
+                <div className="food-truck-review-container">
+                    <div className="rating-container">
+                        rating: {review.rating}
+                    </div>
+                    <div className="review-container">
+                        {review.review}
+                    </div>
+                </div>
+            );
+        });
+
+        return (
+            <div className="transition-item review-page">
+                <div className="food-truck-information">
+                    {this.state.foodTruck.name}
+                    <button onClick={this.goBack.bind(this)}>go back</button>
+                </div>
+                <div className="food-truck-reviews-container">
+                    {reviews}
+                </div>
+            </div>
+        );
+    }
+
+    goBack() {
+        this.props.history.goBack();
+    }
+}
+
+export  default FoodTruckReviews;
